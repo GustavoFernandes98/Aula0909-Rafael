217bd4f7e65604022a4044ab8b2346edb3cdd55f
Gustavo Fernandes <gustavofernandes@MacBook-Pro-de-Gustavo.local>
2026-09-23 21:50:30 -0300

    feat: adiciona DevSecOps ao pipeline (SCA + Trivy) e atualiza mensagem para DevSecOps

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Olá Mundo DevOps!');
+    res.send('Olá Mundo DevsecOps!');
 });
 
 module.exports = app;
